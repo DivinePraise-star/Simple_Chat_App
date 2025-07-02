d73219bfe2287e609b1f351af50d78e900ac23a1
DivinePraise-star <divinepraise699@gmail.com>
2025-07-02 11:20:21 +0600

    added redis and tried to fix bugs

--- simple_chat/chat/consumers.py
+++ simple_chat/chat/consumers.py
@@ -7,44 +7,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
-        user = self.scope["user"]
-
-        # Save message to DB
-        room, _ = ChatRoom.objects.get_or_create(name=self.room_name)
-        if user.is_authenticated:
-            Message.objects.create(room=room, user=user, content=message)
-
-        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': f"{user.username}: {message}" if user.is_authenticated else message
+                'message': message
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(text_data=json.dumps({'message': message}))
--- simple_chat/simple_chat/settings.py
+++ simple_chat/simple_chat/settings.py
@@ -127,8 +127,11 @@ ASGI_APPLICATION = 'simple_chat.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 
 LOGIN_REDIRECT_URL = '/chat/lobby/'
